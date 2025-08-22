2a11c93498514e751ee94028beee434e24f57275
sanderelias <sanderelias+git@gmail.com>
2025-08-22 11:26:50 +0200

    chore: add Dylans text

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+document.addEventListener('mouseover', async (event) => {
+  const { bootstrapApplication } = await import('@angular/platform-browser');
+  const { appConfig } = await import('./app/app.config');
+  const { App } = await import('./app/app');
+
+  bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+}, { once: true });
